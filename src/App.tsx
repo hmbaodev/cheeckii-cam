2647142fe273d6553ba03762e0944c037bb36813
Minh Bao <minhbao@Minhs-Laptop.local>
2025-04-12 20:37:43 +0700

    chore: clear the temporary setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
-import Camera from "./components/camera"
-
 const App = () => {
   return (
-    <div className="w-screen h-screen flex flex-col items-center justify-center bg-gray-200">
-      <p>Camera App</p>
-      <Camera />
+    <div className="flex h-screen w-screen flex-col items-center justify-center bg-gray-200">
+      Cheeckii Cam
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className="flex items-center justify-between">
+      
+    </header>
+  );
+};
+
+export default Header;
