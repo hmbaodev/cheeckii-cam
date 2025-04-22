ae8e152cf0f276c1546b23fb38f1c23e641efa12
Minh Bao <minhbao@Minhs-Laptop.local>
2025-04-22 21:52:42 +0700

    chore: add bg overlay for image items

--- src/routes/Frame.tsx
+++ src/routes/Frame.tsx
@@ -5,13 +5,30 @@ interface FrameProps {
 const Frame = ({ bgImg }: FrameProps) => {
   return (
     <div
-      className="flex aspect-[1/3] w-[180px] flex-col bg-cover bg-center bg-no-repeat px-3 py-5 gap-4"
+      className="relative flex aspect-[1/3] w-[180px] flex-col gap-4 bg-cover bg-center bg-no-repeat px-3 py-5"
       style={{ backgroundImage: `url(${bgImg})` }}
     >
-      <img src="/image.jpg" alt="" className="h-[calc(25%-5px)] w-full object-cover" />
-      <img src="/image.jpg" alt="" className="h-[calc(25%-5px)] w-full object-cover" />
-      <img src="/image.jpg" alt="" className="h-[calc(25%-5px)] w-full object-cover" />
-      <img src="/image.jpg" alt="" className="h-[calc(25%-5px)] w-full object-cover" />
+      <div className="absolute top-0 left-0 z-1 h-full w-full bg-black/15" />
+      <img
+        src="/image.jpg"
+        alt=""
+        className="z-2 h-[calc(25%-5px)] w-full object-cover"
+      />
+      <img
+        src="/image.jpg"
+        alt=""
+        className="z-2 h-[calc(25%-5px)] w-full object-cover"
+      />
+      <img
+        src="/image.jpg"
+        alt=""
+        className="z-2 h-[calc(25%-5px)] w-full object-cover"
+      />
+      <img
+        src="/image.jpg"
+        alt=""
+        className="z-2 h-[calc(25%-5px)] w-full object-cover"
+      />
     </div>
   );
 };
